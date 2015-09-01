f40170a51b1d57293980b2765ebfbd6903f4376e
basilesimon <basile@basilesimon.fr>
2015-09-01 12:28:54 +0100

    Order of install

--- link.sh
+++ link.sh
@@ -6,15 +6,17 @@ do
   ln -f -s $(pwd)/$i ~/$i
 done
 
-# Install ohmyzsh
-echo "Installing oh-my-zsh and themes..."
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-
 # Install Vundle
 # Launch vim and run :PluginInstall
 echo "Installing Vundle..."
 rm -rf ~/dotfiles/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/.vim/bundle
 echo "Launch vim and run :PluginInstall"
+
+# Install ohmyzsh
+echo "Installing oh-my-zsh and themes..."
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+rm ~/.zshrc
+mv ~/.zshrc.pre-oh-my-zsh ~/.zshrc
 
 echo "done."
