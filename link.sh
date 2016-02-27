ef9e6a3c0b9a6c4e970c19b5dd4ad178a01ef3fe
basilesimon <basile@basilesimon.fr>
2016-02-27 15:10:05 +0000

    Deploy works perfectly

--- link.sh
+++ link.sh
@@ -1,5 +1,8 @@
 #/bin/sh
 
+echo "Install git..."
+sudo apt-get install wget git vim
+
 mkdir ~/colorschemes
 echo "Link all dotfiles in /home..."
 for i in $(ls -A -I link.sh)
@@ -15,10 +18,17 @@ rm -rf ~/dotfiles/.vim/bundle/Vundle.vim
 git clone https://github.com/VundleVim/Vundle.vim.git ~/dotfiles/.vim/bundle
 echo "Launch vim and run :PluginInstall"
 
+# Install zsh
+echo "Install zsh..."
+sudo apt-get install zsh
+
 # Install ohmyzsh
 echo "Installing oh-my-zsh and themes..."
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 rm ~/.zshrc ~/.zshrc.pre-oh-my-zsh
 ln -f -s ~/dotfiles/.zshrc ~/.zshrc
+
+echo "Cleaning up..."
+rm -rf ~/.git
 
 echo "done."
