13b131584693b00dfa71de061eddec0f9b25d497
basilesimon <basile@basilesimon.fr>
2018-12-10 22:59:07 +0000

    Change snippets location

--- link_mac.sh
+++ link_mac.sh
@@ -26,7 +26,6 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 
 echo "Importing snippets..."
 rm -r ~/.emacs.d/private/snippets
-# ln -f -s ~/dotfiles/.emacs.d/private/snippets ~/.emacs.d/private/snippets
 
 mkdir ~/colorschemes/
 echo "Link all dotfiles in /home..."
@@ -37,13 +36,15 @@ done
 ln -f -s ~/dotfiles/colorschemes/solarized.py ~/colorschemes/solarized.py
 rm ~/.zshrc ~/.zshrc.pre-oh-my-zsh
 ln -f -s ~/dotfiles/.zshrc ~/.zshrc
+mkdir ~/emacs.d/private
+ln -f -s ~/dotfiles/snippets ~/.emacs.d/private/snippets
 
 # Install Vundle
 # Launch vim and run :PluginInstall
-echo "Installing Vundle..."
-rm -rf ~/dotfiles/.vim/bundle/Vundle.vim
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-echo "Launch vim and run :PluginInstall"
+# echo "Installing Vundle..."
+# rm -rf ~/dotfiles/.vim/bundle/Vundle.vim
+# git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+# echo "Launch vim and run :PluginInstall"
 
 # Install slate
 cd /Applications && curl http://www.ninjamonkeysoftware.com/slate/versions/slate-latest.tar.gz | tar -xz
