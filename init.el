4f95432d84a39ad6a9a96749722ef132b8a1a023
Basile Simon <basile.simon@thomsonreuters.com>
2020-03-25 10:33:32 +0100

    Handle svelte, ejs files in web mode

--- init.el
+++ init.el
@@ -264,6 +264,10 @@
   (progn
     (add-hook 'js2-mode-hook 'prettier-js-mode)))
 
+(use-package web-mode
+  :ensure t
+  :mode (("\\.svelte$" . web-mode)))
+
 (use-package company
   :ensure t
   :defer t
@@ -293,9 +297,9 @@
     (setq-default lorem-ipsum-use-default-bindings)))
 
 ;; R mode
-(use-package ess
-  :ensure t
-  :init (require 'ess-site))
+;; (use-package ess
+;;   :ensure t
+;;   :init (require 'ess-site))
 
 (defun bs/load-init ()
   "Reloads init file"
@@ -399,16 +403,83 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#f4eedb" "#cc1f24" "#778c00" "#a67c00" "#007ec4" "#c42475" "#11948b" "#88999b"])
+ '(compilation-message-face (quote default))
+ '(cua-global-mark-cursor-color "#11948b")
+ '(cua-normal-cursor-color "#596e76")
+ '(cua-overwrite-cursor-color "#a67c00")
+ '(cua-read-only-cursor-color "#778c00")
  '(custom-safe-themes
    (quote
-    ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+    ("c433c87bd4b64b8ba9890e8ed64597ea0f8eb0396f4c9a9e01bd20a04d15d358" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(fci-rule-color "#f4eedb")
+ '(highlight-changes-colors (quote ("#c42475" "#5e65b6")))
+ '(highlight-symbol-colors
+   ("#ed7ddb24b29e" "#cd82e29fd17c" "#fc9acadfb443" "#d974d4beddd6" "#df07dfc6b349" "#f76ccd6eaf2a" "#d132db91e15a"))
+ '(highlight-symbol-foreground-color "#5d737a")
+ '(highlight-tail-colors
+   (quote
+    (("#f4eedb" . 0)
+     ("#a8b84b" . 20)
+     ("#66c1b3" . 30)
+     ("#6fa5e7" . 50)
+     ("#d6a549" . 60)
+     ("#ed6e3e" . 70)
+     ("#f46495" . 85)
+     ("#f4eedb" . 100))))
+ '(hl-bg-colors
+   (quote
+    ("#d6a549" "#ed6e3e" "#ff6243" "#f46495" "#837bdf" "#6fa5e7" "#66c1b3" "#a8b84b")))
+ '(hl-fg-colors
+   (quote
+    ("#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9" "#fffce9")))
+ '(hl-paren-colors (quote ("#11948b" "#a67c00" "#007ec4" "#5e65b6" "#778c00")))
  '(line-number-mode nil)
+ '(nrepl-message-colors
+   (quote
+    ("#cc1f24" "#bb3e06" "#a67c00" "#4f6600" "#a8b84b" "#005797" "#11948b" "#c42475" "#5e65b6")))
  '(org-babel-load-languages (quote ((emacs-lisp . t) (R . t))))
  '(org-confirm-babel-evaluate nil)
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(package-selected-packages
    (quote
-    (ess lorem-ipsum simpleclip company exec-path-from-shell prettier-js evil-mc nlinum-relative diff-hl diminish powerline-evil telephone-line highlight-indent-guides ivy which-key use-package neotree general evil all-the-icons))))
+    (web-mode lorem-ipsum simpleclip company exec-path-from-shell prettier-js evil-mc nlinum-relative diff-hl diminish powerline-evil telephone-line highlight-indent-guides ivy which-key use-package neotree general evil all-the-icons)))
+ '(pos-tip-background-color "#f4eedb")
+ '(pos-tip-foreground-color "#5d737a")
+ '(smartrep-mode-line-active-bg (solarized-color-blend "#778c00" "#f4eedb" 0.2))
+ '(term-default-bg-color "#fffce9")
+ '(term-default-fg-color "#596e76")
+ '(vc-annotate-background nil)
+ '(vc-annotate-background-mode nil)
+ '(vc-annotate-color-map
+   (quote
+    ((20 . "#cc1f24")
+     (40 . "#bbbc5a0718b8")
+     (60 . "#b1ce6c680f6e")
+     (80 . "#a67c00")
+     (100 . "#980682770000")
+     (120 . "#905f85330000")
+     (140 . "#887187c90000")
+     (160 . "#802a8a3b0000")
+     (180 . "#778c00")
+     (200 . "#69798ef83f8d")
+     (220 . "#5f5f904353a2")
+     (240 . "#51b991a0669e")
+     (260 . "#3d7893107927")
+     (280 . "#11948b")
+     (300 . "#1b098bd9a288")
+     (320 . "#1963876fadf3")
+     (340 . "#129882fbb95b")
+     (360 . "#007ec4"))))
+ '(vc-annotate-very-old-color nil)
+ '(weechat-color-list
+   (quote
+    (unspecified "#fffce9" "#f4eedb" "#990001" "#cc1f24" "#4f6600" "#778c00" "#785700" "#a67c00" "#005797" "#007ec4" "#93004d" "#c42475" "#006d68" "#11948b" "#596e76" "#88999b")))
+ '(xterm-color-names
+   ["#f4eedb" "#cc1f24" "#778c00" "#a67c00" "#007ec4" "#c42475" "#11948b" "#002b37"])
+ '(xterm-color-names-bright
+   ["#fffce9" "#bb3e06" "#98a6a6" "#88999b" "#596e76" "#5e65b6" "#5d737a" "#00212b"]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
